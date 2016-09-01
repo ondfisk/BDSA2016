ef7b9717319170f81c1027f66349e789ef4db6ab
Rasmus Lystrøm <rasmus@lystroem.dk>
2016-09-01 15:21:14 +0200

    Lecture 01

---
+++ BDSA2016.Lecture01.Lib.Tests/BankTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Xunit;
+
+namespace BDSA2016.Lecture01.Lib.Tests
+{
+    public class BankTests
+    {
+        [Fact]
+        public void Ctor_sets_balance()
+        {
+            // Arrange
+
+            // Act
+            var bank = new Bank(42);
+
+            // Assert
+            Assert.Equal(42, bank.Balance);
+        }
+
+        [Fact]
+        public void Deposit_increases_balance_by_value()
+        {
+            // Arrange
+            var bank = new Bank();
+
+            // Act
+            bank.Deposit(42);
+
+            // Assert
+            Assert.Equal(42, bank.Balance);
+        }
+
+        [Fact]
+        public void Deposit_given_overflow_throws_OverflowException()
+        {
+            // Arrange
+            var bank = new Bank(42);
+
+            // Act
+
+            // Assert
+            Assert.Throws<OverflowException>(() => bank.Deposit(int.MaxValue));
+        }
+    }
+}
---
+++ BDSA2016.Lecture01.Lib/Bank.cs
@@ -0,0 +1,24 @@
+﻿namespace BDSA2016.Lecture01.Lib
+{
+    public class Bank
+    {
+        public int Balance { get; private set; }
+
+        public Bank()
+        {
+        }
+
+        public Bank(int amount)
+        {
+            Balance = amount;
+        }
+
+        public void Deposit(int amount)
+        {
+            checked
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
