b1b54abf32803e2bd8e8ccbc7e33909645730fea
Rasmus Lystrøm <rasmus@lystroem.dk>
2016-09-01 11:35:41 +0200

    Add project files.

---
+++ BDSA2016.Lecture01.App.Tests/ProgramTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using Xunit;
+
+namespace BDSA2016.Lecture01.App.Tests
+{
+    public class ProgramTests
+    {
+        [Fact]
+        public void Main_when_run_prints_Hello_World()
+        {
+            // Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            // Act
+            Program.Main(null);
+
+            // Assert
+            Assert.Equal("Hello World!", 
+                writer.GetStringBuilder().ToString().TrimEnd());
+        }
+
+        [Fact]
+        public void Main_when_run_with_Brain_prints_Hello_Brain()
+        {
+            // Arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            string[] args = { "Brain" };
+
+            // Act
+            Program.Main(args);
+
+            // Assert
+            Assert.Equal("Hello Brain!",
+                writer.GetStringBuilder().ToString().TrimEnd());
+        }
+    }
+}
---
+++ BDSA2016.Lecture01.App/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+namespace BDSA2016.Lecture01.App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var name = args == null || args.Length == 0 ?
+                "World" :
+                args[0];
+
+            WriteLine($"Hello {name}!");
+        }
+    }
+}
